544b92309eade71476e7687ef944192ce36c7c8b
MJephat <mukundijephat22@gmail.com>
2024-05-05 15:02:44 +0300

    backend done except socket.io

---
+++ backend/controllers/messagecontroller.js
@@ -0,0 +1,65 @@
+const Conversation = require("../models/conversationmodel")
+const Message = require("../models/messagemodel");
+
+exports.sendMessage = async(req, res)=>{
+    try {
+        const { message } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+        
+        let conversation = await Conversation.findOne({
+          particiants: { $all: [senderId, receiverId] },
+        });
+
+        if (!conversation) {
+          conversation = await Conversation.create({
+            participants: [senderId, receiverId],
+          });
+        }
+        
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            message,
+        })
+
+        if(newMessage){
+            conversation.messages.push(newMessage._id);
+        }
+
+        // socket io functionality
+
+        // await conversation.save();
+        // await newMessage.save();
+
+        // the above 2 lines replaced..will hep to run in parallel
+        await Promise.all([conversation.save(), newMessage.save()]);
+
+        res.status(201).json(newMessage);
+
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({error: "internal Server error"});
+        
+    }
+}
+
+exports.getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); // NOT REFERENCE BUT ACTUAL MESSAGES
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller: ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const authRoutes = require('./routers/authroutes.js');
+const messageRoutes = require("./routers/messageroutes.js");
+const userRoutes = require("./routers/userroutes.js");
 const connectDB = require('./db/db.js');
+const cookieParser = require("cookie-parser");
+
 
 const app = express();
 dotenv.config();
@@ -10,8 +14,12 @@ connectDB();
 
 
 app.use(express.json()); // to parse the incoming requests withjson payloads 
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
 
 
 const PORT = process.env.Port || 8000;
