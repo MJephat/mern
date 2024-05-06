194f4910f54933df8c117fcbb4ea4077e91026f1
MJephat <mukundijephat22@gmail.com>
2024-05-06 06:22:38 +0300

    Ui design in progress

---
+++ frontend/vite-project/src/pages/login/UsertypeCheckbox.jsx
@@ -0,0 +1,19 @@
+const UsertypeCheckbox = () => {
+  return (
+    <div className="flex">
+      <div className="form-control">
+        <label className={`label gap-2 cursor-pointer`}>
+          <span className="label-text">customer</span>
+          <input type="checkbox" className="checkbox border-slate-900" />
+        </label>
+      </div>
+      <div className="form-control">
+        <label className={`label gap-2 cursor-pointer`}>
+          <span className="label-text">agent</span>
+          <input type="checkbox" className="checkbox border-slate-900" />
+        </label>
+      </div>
+    </div>
+  );
+};
+export default UsertypeCheckbox;
