5335015180155b5a70522d71ab90782a532c4901
MJephat <mukundijephat22@gmail.com>
2024-05-06 16:50:07 +0300

    midway with frontend

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,16 +1,29 @@
+import { Toaster } from 'react-hot-toast';
 import './App.css'
+import { useAuthContext } from './context/AuthContext';
 import Home from './pages/home/home'
 import Login from './pages/login/login'
+import { Navigate, Route, Routes } from "react-router-dom";
+
 
 function App() {
 
+  const {authUser} = useAuthContext();
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-      {/* <Login /> */}
-      <Home />
-     
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+      </Routes>
+      <Toaster />
     </div>
-  )
+  );
 }
 
 export default App
