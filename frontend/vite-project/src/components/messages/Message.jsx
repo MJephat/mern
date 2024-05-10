5fc6ac7a2d38827da389ccc30dd4d2632849c7a3
MJephat <mukundijephat22@gmail.com>
2024-05-10 19:44:26 +0300

    justify message

--- frontend/vite-project/src/components/messages/Message.jsx
+++ frontend/vite-project/src/components/messages/Message.jsx
@@ -26,7 +26,7 @@ const Message = ({message}) => {
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgcolor} ${shakeClass} pb-2`}>
+      <div className={`chat-bubble justify-start text-white bg-blue-500 ${bubbleBgcolor} ${shakeClass} pb-2`}>
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-enter">
