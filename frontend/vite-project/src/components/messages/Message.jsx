2a1120fff628d92acfd2c035213f0feadb3b76c8
MJephat <mukundijephat22@gmail.com>
2024-05-12 21:21:29 +0300

    styled

--- frontend/vite-project/src/components/messages/Message.jsx
+++ frontend/vite-project/src/components/messages/Message.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
-import {useAuthContext} from '../../context/AuthContext';
-import useConversation from '../../zustand/useConversation';
-import { extractTime } from '../../utils/extractTime';
+// import React from 'react'
+// import {useAuthContext} from '../../context/AuthContext';
+// import useConversation from '../../zustand/useConversation';
+// import { extractTime } from '../../utils/extractTime';
 
-const Message = ({message}) => {
-  const {authUser} = useAuthContext();
-  const {selectedConversation} = useConversation();
-  const formatedTime= extractTime(message.createdAt);
+// const Message = ({message}) => {
+//   const {authUser} = useAuthContext();
+//   const {selectedConversation} = useConversation();
+//   const formatedTime= extractTime(message.createdAt);
+//   const fromMe = message.senderId === authUser._id;
+//   const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+//   const profilePIC = fromMe ? authUser.profilePIC : selectedConversation?.profilePic;
+//   const bubbleBgcolor = fromMe ? "bg-green-400" : "bg-sky-400";
+
+//   const shakeClass = message.shouldShake ? "shake" : "";
+
+//   return (
+//     <div className={`chat ${chatClassName}`}>
+//       <div className="chat-image avatar">
+//         <div className="w-10 rounded-full">
+//           <img
+//             alt="pd"
+//             src={
+//               "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+//             }
+//           />
+//         </div>
+//       </div>
+//       <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgcolor} ${shakeClass} pb-2`}>
+//         {message.message}
+//       </div>
+//       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+//         {formatedTime}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Message
+
+import React from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+import { extractTime } from "../../utils/extractTime";
+
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const formattedTime = extractTime(message.createdAt);
   const fromMe = message.senderId === authUser._id;
-  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
-  const profilePIC = fromMe ? authUser.profilePIC : selectedConversation?.profilePic;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePIC = fromMe
+    ? authUser.profilePIC
+    : selectedConversation?.profilePic;
   const bubbleBgcolor = fromMe ? "bg-green-400" : "bg-sky-400";
 
   const shakeClass = message.shouldShake ? "shake" : "";
@@ -19,21 +61,25 @@ const Message = ({message}) => {
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
-            alt="pd"
+            alt="Profile"
             src={
-              "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+              profilePIC
+                ? profilePIC
+                : "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
             }
           />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500 ${bubbleBgcolor} ${shakeClass} pb-2`}>
+      <div
+        className={`chat-bubble text-white bg-blue-500 ${bubbleBgcolor} ${shakeClass} pb-2`}
+      >
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        {formatedTime}
+        {formattedTime}
       </div>
     </div>
   );
-}
+};
 
-export default Message
+export default Message;
--- frontend/vite-project/src/components/messages/Messages.jsx
+++ frontend/vite-project/src/components/messages/Messages.jsx
@@ -1,37 +1,79 @@
-import { useEffect, useRef } from "react";
+// import { useEffect, useRef } from "react";
+// import useGetMessages from "../../hooks/useGetMessages";
+// import MessageSkeleton from "../skeleton/messageSkeleton";
+// import Message from "./Message";
+// import useListenMessages from "../../hooks/useListenMessages";
+
+// const Messages = () => {
+// 	const {messages, loading} = useGetMessages();
+// 	// console.log("messages", messages)
+// 	useListenMessages();
+// 	//autoscroll messages
+// 	const lastMessageRef = useRef();
+
+// 	useEffect(()=>{
+// 		setTimeout(()=>{
+// 			lastMessageRef.current?.scrollIntoView({behavior: 'smooth'});
+// 		},50);
+// 	},[messages])
+
+// 	return (
+// 		<div className='px-4 flex-1 overflow-auto'>
+// 			{!loading && messages.length > 0 &&
+// 			messages.map((message) =>(
+// 			<div key={message._id} ref={lastMessageRef}>
+// 				<Message message={message} />
+
+// 			</div>
+// 			))}
+
+
+// 			{loading && [...Array(3)].map((_, idx)=><MessageSkeleton key={idx} />)}
+// 			{!loading && messages.length === 0 && (<p className='text-center'>
+// 				Select a Chat to Reply</p>)}
+// 		</div>
+// 	);
+// };
+// export default Messages;
+
+import React, { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeleton/messageSkeleton";
 import Message from "./Message";
 import useListenMessages from "../../hooks/useListenMessages";
 
 const Messages = () => {
-	const {messages, loading} = useGetMessages();
-	// console.log("messages", messages)
-	useListenMessages();
-	//autoscroll messages
-	const lastMessageRef = useRef();
+  const { messages, loading } = useGetMessages();
+  // console.log("messages", messages)
+  useListenMessages();
+  //autoscroll messages
+  const lastMessageRef = useRef();
 
-	useEffect(()=>{
-		setTimeout(()=>{
-			lastMessageRef.current?.scrollIntoView({behavior: 'smooth'});
-		},50);
-	},[messages])
+  useEffect(() => {
+    setTimeout(() => {
+      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, 50);
+  }, [messages]);
 
-	return (
-		<div className='px-4 flex-1 overflow-auto'>
-			{!loading && messages.length > 0 &&
-			messages.map((message) =>(
-			<div key={message._id} ref={lastMessageRef}>
-				<Message message={message} />
+  return (
+    <div className="px-4 flex-1 overflow-auto">
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message, index) => (
+          <div
+            key={message._id}
+            ref={index === messages.length - 1 ? lastMessageRef : null}
+          >
+            <Message message={message} />
+          </div>
+        ))}
 
-			</div>
-			))}
-
-
-			{loading && [...Array(3)].map((_, idx)=><MessageSkeleton key={idx} />)}
-			{!loading && messages.length === 0 && (<p className='text-center'>
-				Select a Chat to Reply</p>)}
-		</div>
-	);
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Select a Chat to Reply</p>
+      )}
+    </div>
+  );
 };
 export default Messages;
+
